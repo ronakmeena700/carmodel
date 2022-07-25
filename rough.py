a6c73f378f168bf532196ab15ae7d249b72b4eb2
ronakmeena700 <107833001+ronakmeena700@users.noreply.github.com>
2022-07-25 11:43:36 +0530

    Update rough.py

--- rough.py
+++ rough.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle
 import numpy as np
-import pandas as pd
+# import pandas as pd
 # from sklearn.tree import DecisionTreeRegressor
 # from sklearn.model_selection import train_test_split
 # from sklearn.compose import ColumnTransformer
